5fcffc53b0a0276f2abba219fac82a5d397c04d6
ethanfields923 <emf209@outlook.com>
2026-03-05 23:46:33 -0500

    Implemented base code to start off
    Includes some basic function definitions that will be needed. More functions will be included later when mapped out properly.

---
+++ Codebase/base.py
@@ -0,0 +1,17 @@
+import matplotlib as plt 
+
+#Scans bank statement pdf
+def pdfScanner():
+    return ""
+
+#Extracts bank transactions from text; will also extract final account balance
+def textExtracter():
+    return ""
+
+#Generates a .csv file using the extracted banking information
+def csvGenerator():
+    return ""
+
+#Main method; Calls on the other functions above
+def main():
+    print("Hello World")
